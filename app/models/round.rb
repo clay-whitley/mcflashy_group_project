001ab59bd2d54cd27cf9c21c74c758a636776144
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-09 15:58:05 -0700

    create logic to choose card

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,13 @@
 class Round < ActiveRecord::Base
-  # Remember to create a migration!
+  
+  def grab_next_card
+    played_cards = []
+    all_cards_in_deck = self.deck.cards.all
+    all_cards_in_deck.map! { |card| card.id }
+    self.attempts.each do |attempt|
+      played_cards << attempt.card_id
+    end
+    (all_cards_in_deck - played_cards).sample
+  end
+
 end
