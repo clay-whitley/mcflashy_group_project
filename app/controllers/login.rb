5267ff6f6a3c9fae6f59f2b3a0930f762b6cf580
Apprentice <apprentice@dbc09.local>
2013-08-09 15:05:36 -0700

    finished user auth view and route wireframes

---
+++ app/controllers/login.rb
@@ -0,0 +1,15 @@
+# get '/login' do 
+#     
+  
+# end
+post '/login' do
+  #logic to sign in user (authenticate)
+  #erb :list
+end
+
+get '/sign_out' do
+  #logic to sign out user
+  erb :index
+end
+
+
---
+++ app/controllers/user_create.rb
@@ -0,0 +1,13 @@
+# get '/sign_up' do
+
+# end
+
+post '/sign_up' do
+  #authenticate with validation
+  #create new user
+  #erb :list
+end
+
+# get '/create_user' do
+
+# end
