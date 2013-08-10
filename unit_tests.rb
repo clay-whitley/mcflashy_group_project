55a5ce8190bfb8f7b228db52ad5fab4423710c1e
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-10 16:10:15 -0700

    update unit tests to test for user authentication and rounds.

--- unit_tests.rb
+++ unit_tests.rb
@@ -7,15 +7,27 @@ def assert(boolean, message)
   puts
 end
 
+# Destroys users between tests.
+
+User.where(last_name: "User").destroy_all
+
+user_1 = User.new(email: "user1@users.com", first_name: "First", last_name: "User")
+user_1.password = "password1"
+user_1.save
+user_2 = User.new(email: "user2@users.com", first_name: "Second", last_name: "User")
+user_2.password = "password2"
+user_2.save
+
 deck_1 = Deck.find(1)
 deck_2 = Deck.find(2)
 deck_3 = Deck.find(3)
 card_1 = deck_1.cards.first
 card_2 = deck_2.cards.first
 card_3 = deck_3.cards.first
-round_1 = deck_1.rounds.create(user_id: 1)
+round_1 = deck_1.rounds.create(user_id: user_2.id)
 attempt_1 = round_1.attempts.create(card_id: 1, input: "Sacramento")
 attempt_2 = round_1.attempts.create(card_id: 1, input: "San Francisco")
+
 
 # These tests assume that the seed file has already been ran
 
@@ -28,5 +40,7 @@ assert(card_3.term == "Ginko", "The first card in the third deck has the right n
 assert(deck_1.cards.map { |card| card.id }.include?(round_1.grab_next_card), "The round pulls the correct next card")
 assert(attempt_1.outcome, "The outcome of the first attempt should be true")
 assert(attempt_2.outcome == false, "The second attempt should be false")
+assert(User.authenticate("user1@users.com", "password1").id == user_1.id, "User successfully authenticates with correct password")
+assert(User.authenticate("user1@users.com", "password").nil?, "User is not authenticated with incorrect password")
+assert(user_2.rounds.any?, "User has rounds associated with it")
 
-p round_1
